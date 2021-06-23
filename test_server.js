931b330adbaef6b536ec6c40312d1493c1788a5f
pablopenrose <local@kader.email>
2021-06-23 16:50:48 -0400

    adding form submission to spreadsheet done

--- test_server.js
+++ test_server.js
@@ -6,19 +6,60 @@ const app = express()
 
 const bodyParser = require ('body-parser')
 
+const GoogleSpreadsheet = require ('google-spreadsheet')
+const { promisify } = require ('util')
+const creds = require ('./client_secret.json')
+
 app.use(express.static(path.join(__dirname, 'build')))
+app.use(bodyParser.json({ type: 'application/*+json' }))
 
 app.get ('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 
 })
 
+async function accessSpreadsheet (row) {
+    const doc = new GoogleSpreadsheet ('1g1rlRfpDwMkoQCjQYDRu0tWOk0TTzPOAbvXcUHFXDh8')
+    await promisify (doc.useServiceAccountAuth)(creds)
+    const info = await promisify(doc.getInfo)()
+    const sheet = info.worksheets[0]
+
+    await promisify(sheet.addRow)(row)
+    //console.log(sheet)
+}
+
 var encodedParser = bodyParser.urlencoded({extended: false})
 
 app.post('/', encodedParser, function (req, res) {
     if (!req.body) return res.sendStatus(400)
-    console.log(req.body)
+    var body = JSON.stringify(req.body)
+    body = body.replaceAll("\\", "")
+    body = body.replaceAll("{", "")
+    body = body.replaceAll("}", "")
+    body = body.replaceAll(":", "")
+    body = body.replaceAll(",", "")
+    var li = body.split("\"")
+    let a = []
+    for (var l of li) {
+        if (l.length > 0) {
+            if (l !== 'name' && l !== 'email' && l !== 'student' && l !== 'age') {
+                a.push(l)
+            }
+        }
+    }
+
+    var row = {
+        name: a[0],
+        email: a[1],
+        student: a[2], 
+        age: a[3]
+    }
+
+    console.log(row)
+    accessSpreadsheet(row)
+    
 })
 
 app.listen(PORT)
+
 
